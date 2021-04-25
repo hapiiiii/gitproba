f7949cfed2ade29d9432b55510e824f43cf5ed47
hapiiiii <61937457+hapiiiii@users.noreply.github.com>
2021-04-25 16:11:48 +0200

    nova promjna

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,7 +6,5 @@ import javafx.scene.control.Label;
 public class Controller {
     public Label lqbelq;
 
-    public void ok(ActionEvent actionEvent) {
-        lqbelq.setText("      ok");
-    }
+
 }
