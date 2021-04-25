fd1071fc0bfbe170315182009e6f78f9c08e08c9
hapiiiii <61937457+hapiiiii@users.noreply.github.com>
2021-04-25 16:07:58 +0200

    proba

---
+++ src/sample/Controller.java
@@ -0,0 +1,12 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class Controller {
+    public Label lqbelq;
+
+    public void ok(ActionEvent actionEvent) {
+        lqbelq.setText("      ok");
+    }
+}
